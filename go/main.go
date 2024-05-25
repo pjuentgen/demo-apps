8005c1605758fa65622a5bde336171e75da7f1b7
Peter Jüntgen <peter.juentgen@juentgen.net>
2024-05-25 15:42:44 +0200

    extended readme and added consistency

--- go/main.go
+++ go/main.go
@@ -2,37 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"math/rand/v2"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Request to /")
-		randomNumber := rand.IntN(500)
-		time.Sleep(time.Millisecond * time.Duration(randomNumber))
-		if randomNumber > 300 {
-			fmt.Fprintf(w, "Hello World!")
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Error occured"))
-		}
 
-	})
+	
+	http.HandleFunc("/", handleDefault)
 
-	http.HandleFunc("/random-error", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Request to /random-error")
-		randomNumber := rand.IntN(500)
-		time.Sleep(time.Millisecond * time.Duration(randomNumber))
-		if randomNumber > 300 {
-			fmt.Fprintf(w, "Hello World!")
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Error occured"))
-		}
-
-	})
+	http.HandleFunc("/random-error", handleDefault)
 
 	http.HandleFunc("/bad", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Request to /bad")
@@ -46,17 +29,68 @@ func main() {
 		fmt.Fprintf(w, "Hello World!")
 	})
 
-	http.HandleFunc("/remote", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "To be implemented!")
+	http.HandleFunc("/remote", func(w http.ResponseWriter, r *http.Request) {		
+		fmt.Println("Request to /remote")
+		url := os.Getenv("REMOTE_URL")
+		if url == "" {
+			url = "https://api.chucknorris.io/jokes/random"
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error occurred"))
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error occurred"))
+			return
+		}
+
+		fmt.Fprint(w, string(body))
 	})
 
 	http.HandleFunc("/readiness", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Ready!")
+		fmt.Fprintf(w, "Ready")
 	})
 
 	http.HandleFunc("/liveness", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Live!")
+		fmt.Fprintf(w, "Live")
 	})
 
 	http.ListenAndServe(":8080", nil)
 }
+
+func handleDefault(responseWriter http.ResponseWriter, request *http.Request) {	
+		fmt.Println("Request to /")		
+		timeEnv := os.Getenv("TIME")
+		var timeInt int = 500
+		var timeErrorInt int = 300
+		if timeEnv != "" {
+			t, err := strconv.Atoi(timeEnv)
+			if err == nil {
+				timeInt = t
+			}	
+		}
+		timeErrorEnv := os.Getenv("TIME_FOR_ERROR")		
+		if timeErrorEnv != "" {
+			t, err := strconv.Atoi(timeErrorEnv)
+			if err == nil {
+				timeErrorInt = t
+			}	
+		}
+
+		randomNumber := rand.IntN(timeInt)
+		time.Sleep(time.Millisecond * time.Duration(randomNumber))
+		if randomNumber > timeErrorInt {
+			fmt.Fprintf(responseWriter, "Hello World!")
+		} else {
+			responseWriter.WriteHeader(http.StatusInternalServerError)
+			responseWriter.Write([]byte("Error occured"))
+		}
+	}
+
