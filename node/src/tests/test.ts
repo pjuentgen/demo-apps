8d0a663037ad7bafd2d3833dc376cb3a4d378660
Peter Juentgen <peter.juentgen@juentgen.net>
2024-05-25 23:42:19 +0200

    typing disabled for logger for the moment. meter not set globally (will probably be done by auto instru)

--- node/src/tests/test.ts
+++ node/src/tests/test.ts
@@ -6,20 +6,8 @@ import app from '../app';
 const requestWithSupertest = supertest(app);
 
 describe('Basic Test', () => {
-  // it('GET /', async () => {
-  //   const res = await requestWithSupertest.get('/healthy');
-  //   expect(res.status).;
-  // });
-  it('GET /healthy', async () => {
+  it('GET /', async () => {
     const res = await requestWithSupertest.get('/healthy');
     expect(res.status).toEqual(200);
-    // expect(res.type).toEqual(expect.stringContaining('json'));
-    // expect(res.body).toHaveProperty('users')
-  });
-  it('GET /bad', async () => {
-    const res = await requestWithSupertest.get('/bad');
-    expect(res.status).toEqual(500);
-    // expect(res.type).toEqual(expect.stringContaining('json'));
-    // expect(res.body).toHaveProperty('users')
   });
 });
