ccbb99cb0f143fb95ab0b694fda71398619f2589
Peter Juentgen <pjuentgen@users.noreply.github.com>
2024-11-21 22:58:14 +0100

    Update server.rb

--- ruby/server.rb
+++ ruby/server.rb
@@ -13,16 +13,6 @@ get '/health' do
   "Healthy"
 end
 
-get '/cowsay' do
-  content_type "text/plain"
-  message = params[:message]
-  if message == nil
-    message = "Set a message by adding ?message=<message here> to the URL"
-  end
-
-  Cowsay.say(message, "random")
-end
-
 
 get '/liveness' do
   content_type "text/plain"
@@ -68,57 +58,4 @@ end
 get '/' do
   content_type "text/plain"
   "Hello World!"
-end
-
-get '/liveness' do
-  content_type "text/plain"
-  "Live"
-end
-
-get '/readiness' do
-  content_type "text/plain"
-  "Ready"
-end
-
-get '/remote' do
-  content_type "application/json"
-  require 'net/http'
-  require 'json'
-  url = ENV["REMOTE_URL"] || "http://localhost:8080"
-  uri = URI(url)
-  response = Net::HTTP.get(uri)
-  response
-end
-
-get '/healthy' do
-  content_type "text/plain"
-  "Hello World!"
-end
-
-get '/bad' do
-  content_type "text/plain"
-  status 500
-  "Error occurred"
-end
-
-get '/random-error' do
-  content_type "text/plain"
-  if rand(1..2) == 1
-    status 500
-    "Error occurred"
-  else
-    "Hello World!"
-  end
-end
-
-
-
-get '/' do
-  content_type "text/plain"
-  message = params[:message]
-  if message == nil
-    message = "Set a message by adding ?message=<message here> to the URL"
-  end
-
-  Cowsay.say(message, "random")
 end
