3b75ab6c576e93e68bc5c4da01d70ddc2ba0ba59
Peter Juentgen <peter.juentgen@juentgen.net>
2025-04-10 03:36:03 +0200

    winston logger example for nodejs demo app

--- node/src/logger.ts
+++ node/src/logger.ts
@@ -1,11 +1,26 @@
-import pino from 'pino';
+import { createLogger, format, transports } from 'winston';
+import { trace } from '@opentelemetry/api';
 
-export const logger = pino({
-  formatters: {
-    level: (label) => {
-      return {
-        level: label
-      };
-    }
+const addTraceIds = format((info) => {
+  const currentSpan = trace.getActiveSpan();
+  if (currentSpan) {
+    const spanContext = currentSpan.spanContext();
+    info.traceId = spanContext.traceId;
+    info.spanId = spanContext.spanId;
   }
+  return info;
+});
+
+export const logger = createLogger({
+  format: format.combine(
+    addTraceIds(),
+    format.timestamp(),
+    format.printf(({ timestamp, level, message, traceId, spanId }) => {
+      return `${timestamp} [${level}] traceId=${traceId} spanId=${spanId} ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(), // Logs to the console
+    // Add other transports like File if needed
+  ],
 });
--- node/src/server.ts
+++ node/src/server.ts
@@ -4,4 +4,4 @@ import { logger } from './logger';
 const PORT: number = parseInt(process.env.PORT || '8080');
 
 // eslint-disable-next-line no-console
-app.listen(PORT, (): void => logger.info(`running on port ${PORT}`));
+app.listen(PORT, (): void => { logger.info(`Server is running on port ${PORT}`); });
